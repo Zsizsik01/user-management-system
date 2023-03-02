69fa7bddb7e6f6aaca123f4050dd897d3ff59ef7
Zsolt Nyeste <zsolt.nyeste@spreadmonitor.com>
2023-03-02 17:52:14 +0100

    fix: more accurate task solution

--- routes/user.py
+++ routes/user.py
@@ -4,64 +4,128 @@ from config.db import conn
 from schemas.user import serializeDict, serializeList
 from bson import ObjectId
 from datetime import datetime
+import bcrypt
 
 user = APIRouter()
 
 @user.get('/login')
 async def login(email, password):
-    conn.local.user.find_one_and_update({"email": email, "password": password}, {
-        "$set": {
-            "updatedAt": datetime.today()
-        }
-    })
+    '''
+    This function logs in the user with the provided email and password.
+    :param email: Email address of the user.
+    :param password: Password of the user.
+    :return: With error message if the email or the password is incorrect. With success message if the login was successful.
+    '''
 
-    user = conn.local.user.find_one({"email": email, "password": password})
+    bytes = password.encode('utf-8')
 
-    if user is not None:
-        return serializeDict(user)
-    else:
-        return -1
+    user = conn.local.user.find_one({'email': email})
+
+    if user is None:
+        return 'The email is incorrect!'
+
+    if bcrypt.checkpw(bytes, serializeDict(user)['password']):
+        conn.local.user.find_one_and_update({'email': email}, {
+            '$set': {
+                'updatedAt': datetime.today()
+            }
+        })
+
+        return 'Login was successful!'
+
+    return 'The password is incorrect!'
 
 @user.get('/')
 async def find_all_users():
+    '''
+    This function lists all existing users.
+    :return: With the list of users.
+    '''
+
     return serializeList(conn.local.user.find())
 
 @user.get('/{id}')
 async def find_one_user(id):
-    if len(id) != 24:
-        return -1
+    '''
+    This function gets a selected user by id.
+    :param id: The ID of the selected user.
+    :return: With the selected user. With error message if the ID is not valid or the user with the ID does not exist.
+    '''
 
-    user = conn.local.user.find_one({"_id": ObjectId(id)})
+    if ObjectId.is_valid(id) == False:
+        return 'Object ID is not valid!'
+
+    user = conn.local.user.find_one({'_id': ObjectId(id)})
 
     if user is not None:
         return serializeDict(user)
     else:
-        return -1
+        return 'User not found!'
 
 @user.post('/')
 async def create_user(user: User):
+    '''
+    This function creates a new user.
+    :param user: A user object which contains name, email, password and updatedAt data.
+    :return: With the newly created user. With error message if there is already a user with this email address.
+    '''
+
+    bytes = user.password.encode('utf-8')
+    user.password = bcrypt.hashpw(bytes, bcrypt.gensalt())
+
+    userFromDatabase = conn.local.user.find_one({'email': user.email})
+
+    if userFromDatabase is not None:
+        return 'There is already a user with this email address!'
+
     conn.local.user.insert_one(dict(user))
+
     return serializeList(conn.local.user.find())
 
 @user.put('/{id}')
 async def update_user(id, user: User):
-    if len(id) != 24:
-        return -1
+    '''
+    This function updates the selected user.
+    :param id: The ID of the selected user.
+    :param user: A user object which contains name, email, password and updatedAt data.
+    :return: With the updated user. With error message if the ID is not valid or the user is not found with the selected ID.
+    '''
 
-    conn.local.user.find_one_and_update({"_id": ObjectId(id)}, {
-        "$set": dict(user)
+    if ObjectId.is_valid(id) == False:
+        return 'Object ID is not valid!'
+
+    bytes = user.password.encode('utf-8')
+    user.password = bcrypt.hashpw(bytes, bcrypt.gensalt())
+
+    del user.updatedAt
+
+    conn.local.user.find_one_and_update({'_id': ObjectId(id)}, {
+        '$set': dict(user)
     })
 
-    updatedUser = conn.local.user.find_one({"_id": ObjectId(id)})
+    updatedUser = conn.local.user.find_one({'_id': ObjectId(id)})
 
     if updatedUser is not None:
         return serializeDict(updatedUser)
     else:
-        return -1
+        return 'User not found!'
 
 @user.delete('/{id}')
 async def delete_user(id):
-    if len(id) != 24:
-        return -1
+    '''
+    This function deletes a selected user.
+    :param id: The ID of the selected user.
+    :return: With success message. With error message if the ID is not valid or the user is not found with the selected ID.
+    '''
 
-    user = conn.local.user.find_one_and_delete({"_id": ObjectId(id)})
+    if ObjectId.is_valid(id) == False:
+        return 'Object ID is not valid!'
+
+    user = conn.local.user.find_one({'_id': ObjectId(id)})
+
+    if user is None:
+        return 'User does not exist!'
+
+    conn.local.user.find_one_and_delete({'_id': ObjectId(id)})
+
+    return 'The deletion was successful!'
--- schemas/user.py
+++ schemas/user.py
@@ -1,5 +1,11 @@
-def serializeDict(a) -> dict:
-    return {**{i:str(a[i]) for i in a if i=='_id'}, **{i:a[i] for i in a if i!='_id'}}
+def serializeDict(item) -> dict:
+    return {
+        'id':str(item['_id']),
+        'name':item['name'],
+        'email':item['email'],
+        'password':item['password'],
+        'updatedAt':item['updatedAt']
+    }
 
 def serializeList(entity) -> list:
-    return [serializeDict(a) for a in entity]
+    return [serializeDict(item) for item in entity]
